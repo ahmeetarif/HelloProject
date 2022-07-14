1bf2a9867238c1b94f24ac30ad02c9cc6b520197
ahmeetarif <ahmetarif1254@gmail.com>
2022-07-14 17:47:18 +0300

    Add project files.

---
+++ HelloProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
